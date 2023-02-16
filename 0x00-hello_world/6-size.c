1ca615d49939cae3fb0a72c07c323a9a84c1a886
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-16 04:14:46 -0800

    added the 6 file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int lli;
 	float fl;
 {   
-	printf("Size of a char: %d byte(s)\n", sizeOf(ch));
-	printf("Size of a int: %d byte(s)\n", sizeOf(in));
-	printf("Size of a long int: %d byte(s)\n", sizeOf(li));
-	printf("Size of a long long int: %d byte(s)\n", sizeOf(lli));
-	printf("Size of a float: %d byte(s)\n", sizeOf(fl));
+	printf("Size of a char: %d byte(s)\n", sizeof(ch));
+	printf("Size of a int: %d byte(s)\n", sizeof(in));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of a float: %d byte(s)\n", sizeof(fl));
 	return (0);
 }
