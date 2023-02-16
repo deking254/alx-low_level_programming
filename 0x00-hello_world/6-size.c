99bfc33e5b96054cb7a0d226a8f26a730155ac44
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-16 03:57:37 -0800

    added the 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,21 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	char ch;
+	int in;
+	long int li;
+	long long int lli;
+	float fl;
+{   
+	printf(Size of a char: %d byte(s), sizeOf(ch));
+	printf(Size of a int: %d byte(s), sizeOf(in));
+	printf(Size of a long int: %d byte(s), sizeOf(li));
+	printf(Size of a long long int: %d byte(s), sizeOf(lli));
+	printf(Size of a float: %d byte(s), sizeOf(fl));
+	return (0);
+}
