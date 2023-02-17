7643bf778c861bcf740b720379e5937eefea8f04
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-17 05:53:21 -0800

    added the 4-print file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char f;
+
+	for (f = 'a'; f <= 'z'; ++f)
+	{
+	if (f != 'e' ||  f != 'q')
+	putchar(f);
+	}
+}
