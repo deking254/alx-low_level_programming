fa69913549d504cdd9413993a078ac5fdfc740ea
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-17 05:03:14 -0800

    ddeiadde

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,18 +14,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of ");
-	printf("%d ",n);
-	if ((n%10) > 5)
+	printf("%d ", n);
+	if ((n % 10) > 5)
 	{
-	printf("is %d and is greater than 5\n",n%10);
+	printf("is %d and is greater than 5\n", n % 10);
 	}
-	else if ((n%10) == 0)
+	else if ((n % 10) == 0)
 	{
-	printf("is %d and is 0\n",n%10);
+	printf("is %d and is 0\n", n % 10);
 	}
-	else if ((n%10) < 6 && n != 0)
+	else if ((n % 10) < 6 && n != 0)
 	{
-	printf("is %d and is less than 6 and not 0\n",n%10);
+	printf("is %d and is less than 6 and not 0\n", n % 10);
 	}
 	return (0);
 }
