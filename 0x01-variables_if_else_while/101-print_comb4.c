6a35d55e84e2fe920ddae47ded8b9b424202939e
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-17 15:07:08 -0800

    added the 101-print file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int c, b;
+
+	for (a = 0; a <= 7; a++)
+	{
+	for (b = a + 1; b <= 8; b++)
+	{
+	for (c = a + 2 ; c <= 9 ; c++)
+	{
+	putchar(a + '0');
+	putchar(b + '0');
+	putchar(c + '0');
+	if (a < 7)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
