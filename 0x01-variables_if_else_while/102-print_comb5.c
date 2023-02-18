cb0a70fd55b2e7ce84220e35f2579c74f9f38d73
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-18 07:24:58 -0800

    added a 102-print file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int c, b, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+	for (b = 0; b <= 8; b++)
+	{
+	for (d = 0; d <= 9; d++)
+	{
+	for (c = 1  ; c <= 9 ; c++)
+	{
+	putchar(a + '0');
+	putchar(b + '0');
+	if (b < 7)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	putchar(d + '0');
+	putchar(c + '0');
+	}
+	}
+	}
+	}
+	return (0);
+}
