c1036b46f1fcb798e38ca0d359d6dc3df4d6df71
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-19 09:05:12 -0800

    102 print file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,27 +9,25 @@
 int main(void)
 {
         int a, d;
-        int b;
 
         for (a = 0; a <= 9; a++)
         {
-	for (d = 0; d <= 8; d++)
+	for (b = 0; a <= 8; b++)
 	{
-        for (b = 1; b <= 99; b++)
-        {
-	putchar(a + '0');
+	for (d = 1; d <= 99; d++)
+	{
+	if (d < 10)
+	{
+	putchar('0');
         putchar(d + '0');
+	} else if (d => 10)
+	{
+	putchar((d / 10 % 10 + '0');
+	putchar((d % 10) + '0');
+	}
         putchar(' ');
-	if (b < 10)
-	{
-	putchar('0');
-	putchar(b +'0');
-	}
-	else
-	{
-	putchar('0');
-	putchar(b % 10 + '0');
-	}
+	putchar(a + '0');
+	putchar(b + '0');
 	if (d < 8)
 	{
 	putchar(',');
