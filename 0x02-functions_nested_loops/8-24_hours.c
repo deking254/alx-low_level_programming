1af03346673e32608561be4caccf653fa790444f
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-21 16:43:10 -0800

    some changes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,11 +14,28 @@ void jack_bauer(void)
 	{
 	while (j <= 59)
 	{
+	if ( i > 10)
+	{
+	_putchar((i / 10) % 10);
+	_putchar(i % 10);
+	}
+	else
+	{
 	_putchar('0');
 	_putchar(j + '0');
+	}
+	_putchar(':');
+	if (i < 10)
+	{
+	_putchar('0');
+	_putchar(j + '0');
+	}
+	else
+	{
 	_putchar(':');
 	_putchar((i / 10) % 10);
 	_putchar(i % 10);
+	}
 	j++;
 	_putchar('\n');
 	}
