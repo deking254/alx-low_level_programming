9a01c15372cea646d8aca18e1985f2d65ae023e0
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-23 11:55:15 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,13 +9,13 @@ void print_diagonal(int n)
 {
 	int i;
 
-	if (i <= 0)
+	if (n <= 0)
 	_putchar('\n');
 	else
 	{
 	for (i = 0; i < n; i++)
 	{
-	_putchar('b');
+	_putchar(92);
 	}
 	_putchar('\n');
 	}
