625d9ccba5fa3e13af9d723b5948cc5b8f578947
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-23 11:28:31 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ * @n: integer
+ * Return: Always void.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+	_putchar('\');
+	_putchar('n');
+	}
+	else
+	{
+	for (i = 0; i < n; i++)
+	{
+	_putchar(95);
+	}
+	_putchar('\');
+	_putchar('n');
+	}
+}
