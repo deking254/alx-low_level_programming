23ac44bc9300047c7683e7fbf2bbb79511f4b014
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-23 12:29:27 -0800

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - check the code
+ * @size: integer
+ * Return: Always void.
+ */
+void print_square(int size)
+{
+	int i;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (j = 0; j < n; j++)
+	{
+	for (i = 0; i < n; i++)
+	_putchar(35);
+	_putchar('\n');
+	}
+	}
+
+
+}
