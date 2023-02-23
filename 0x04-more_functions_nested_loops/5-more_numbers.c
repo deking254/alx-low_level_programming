eacbe663f4448c693bc42a3ee1389b1565a37b82
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-23 08:24:49 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,11 @@ void more_numbers(void)
 	for (j = 0; j <= 9; j++)
 	{
 	for (i = 0; i < 15;  i++)
+	{
 	if (i > 9)
 	_putchar((i / 10) + '0');
 	_putchar((i % 10) + '0');
+	}
 	_putchar('\n');
 	}
 }
