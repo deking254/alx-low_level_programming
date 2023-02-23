e878ea39c404a54156ec1d14cbb7780901019344
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-23 07:55:35 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - code
+ * more_numbers - check the code
+ * Return: Always void.
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 9; j++)
+	{
+	for (i = 0; i < 15;  i++)
+	_putchar(i + '0');
+	}
+	_putchar('\n');
+}
