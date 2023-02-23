ec53b4496af53667a6280861e8e7bc314e21e550
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-23 12:31:24 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,13 +15,11 @@ void print_square(int size)
 	}
 	else
 	{
-	for (j = 0; j < n; j++)
+	for (j = 0; j < size; j++)
 	{
-	for (i = 0; i < n; i++)
+	for (i = 0; i < size; i++)
 	_putchar(35);
 	_putchar('\n');
 	}
 	}
-
-
 }
