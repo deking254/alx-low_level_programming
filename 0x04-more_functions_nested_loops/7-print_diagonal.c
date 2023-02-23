f7ff14243f9422bcfd4bc5c984edaedbd6e754b7
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-23 11:50:13 -0800

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ * @n: integer
+ * Return: Always void.
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (i <= 0)
+	_putchar('\n');
+	else
+	{
+	for (i = 0; i < n; i++)
+	{
+	_putchar(92);
+	}
+	_putchar('\n');
+	}
+}
