dbf0d97ecbdb8fe177c90946569e7b289433f37e
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-27 05:20:33 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,13 +2,22 @@
 
 /**
  * *_strcpy - check the code
- * @*dest: pointer a
- * @*src: pointer src n
+ * @dest: pointer a
+ * @src: pointer src n
  * Return: Always void.
  */
 char *_strcpy(char *dest, char *src)
 {
+int i;
 
-
-
+for (i = 0; ; i++)
+{
+if (src[i] == '\0')
+{
+dest[i] = src[i];
+break;
+}
+dest[i] = src[i];
+}
+return (dest);
 }
