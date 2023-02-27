1ff3c99ec1893d352f505cf0752b3d4c46bc1fb5
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-27 04:26:54 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,18 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * puts2 - check the code
- * @*str: pointer c
+ * @str: pointer c
  * Return: Always void.
  */
 void puts2(char *str)
 {
+int i, j;
+
+j = strlen(str);
+for (i = 0; i <= j; i += 2)
+_putchar(str[i]);
+_putchar('\n');
 
 
 
