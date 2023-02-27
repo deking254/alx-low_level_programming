6a1bfcedf0f87f324da28fd4452fcdbb4b211a37
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-27 04:01:12 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,17 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * print_rev - check the code
- * @*s: pointer c
+ * @s: pointer c
  * Return: Always void.
  */
 void print_rev(char *s)
 {
+	int i, j;
 
-
-
+	j = strlen(s);
+	for(i = j; i >= 0; i--)
+	_putchar(s[i]);
+	_putchar('\n');
 }
