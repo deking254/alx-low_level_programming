fc416baaa3b7ab767597938cf65b7218f5014b8e
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-02-27 04:18:24 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,15 +1,16 @@
-
 #include "main.h"
+#include <string.h>
 
 /**
  * rev_string - check the code
- * @*s: pointer c
+ * @s: pointer c
  * Return: Always void.
  */
 void rev_string(char *s)
 {
+	int i, j;
 
-
-
+	j = strlen(s);
+	for (i = j; i >= 0; i--)
+	_putchar(s[i]);
 }
-
