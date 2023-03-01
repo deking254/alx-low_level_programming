165ed86fae30de8af9db680920bee0dc57b13f94
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-01 05:06:53 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,24 @@
-#include 'main.h'
+#include "main.h"
+
+/**
+ * _strncpy - check the code
+ * @dest: integer operand.
+ * @src: integer operand.
+ * @n: integer operand.
+ * Return: Always int.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+int srclen = 0;
+
+while (src[i++])
+{
+srclen++;
+}
+for (i = 0; src[i] && i < n; i++)
+dest[i] = src[i];
+for (i = srclen; i < n; i++)
+dest[i] = '\0';
+return (dest);
+}
