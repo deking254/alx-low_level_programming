c385069d7dc04820de83ce470dc171161d4af8ae
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-01 05:00:50 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,22 @@
-#include 'main.h'
+#include "main.h"
+
+/**
+ * _strncat - check the code
+ * @dest: integer operand.
+ * @src: integer operand.
+ * @n: integer operand.
+ * Return: Always int.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+int destlen = 0;
+
+while (dest[i++])
+{
+destlen++;
+}
+for (i = 0; src[i] && i < n; i++)
+dest[destlen++] = src[i];
+return (dest);
+}
