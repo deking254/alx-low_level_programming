0a1857931075947a34bd46e56acd92bea4cb7fbc
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-01 07:05:14 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,24 @@
-#include 'main.h'
+#include "main.h"
+
+/**
+ * leet - check the code
+ * @str: integer operand.
+ * Return: Always int.
+ */
+char *leet(char *str)
+{
+int i1 = 0;
+int i2;
+char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+while (str[i1])
+{
+for (i2 = 0; i2 <= 7; i2++)
+{
+if (str[i1] == leet[i2] || str[i1] -32 == leet[i2])
+str[i1] = i2 + '0';
+}
+i1++;
+}
+return (str);
+}
