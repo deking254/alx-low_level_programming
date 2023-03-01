a5fea8ed92e8c55aa08faf4634dc8e5a387f75a2
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-01 05:21:58 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,17 @@
-#include 'main.h'
+#include "main.h"
+
+/**
+ * _strcmp - check the code
+ * @s1: integer operand.
+ * @s2: integer operand.
+ * Return: Always int.
+ */
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 && *s2 && *s1 == *s2)
+{
+s1++;
+s2++;
+}
+return (*s1 - *s2);
+}
