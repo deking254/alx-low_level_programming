831f953a92bc5021a8f302a67bedb8f055ff2351
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-01 05:53:55 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,19 @@
-#include 'main.h'
+#include "main.h"
+
+/**
+ * string_toupper - check the code
+ * @str: integer operand.
+ * Return: Always int.
+ */
+char *string_toupper(char *str)
+{
+int  i = 0;
+
+while (str[i])
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] -= 32;
+i++;
+}
+return (str);
+}
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,33 @@
-#include 'main.h'
+#include "main.h"
+
+/**
+ * cap_string - check the code
+ * @str: integer operand.
+ * Return: Always int.
+ */
+char *cap_string(char *str)
+{
+int i = 0;
+while (str[i])
+{
+while (!(str[i] >= 'a' && str[i] <= 'z'))
+i++;
+if (str[i - 1] == ' ' ||
+str[i - 1] == '\t' ||
+str[i - 1] == '\n' ||
+str[i - 1] == ',' ||
+str[i - 1] == ';' ||
+str[i - 1] == '.' ||
+str[i - 1] == '!' ||
+str[i - 1] == '?' ||
+str[i - 1] == '"' ||
+str[i - 1] == '{' ||
+str[i - 1] == '}' ||
+str[i - 1] == '{' ||
+str[i - 1] == '}' ||
+i == 0)
+str[i] -= 32;
+i++;
+}
+return (str);
+}
