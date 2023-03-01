2f394b1b797fdef91047dc816fc33e7ee95397dd
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-01 05:34:38 -0800

    "4-rev_array.c"

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,18 @@
-#include 'main.h'
+#include "main.h"
+
+/**
+ * reverse_array - check the code
+ * @a: integer operand.
+ * @n: integer operand.
+ * Return: Always int.
+ */
+void reverse_array(int *a, int n)
+{
+int t, i;
+for (i = n - 1; i >= n / 2; i--)
+{
+t = a[n - 1 - i];
+a[n - 1 - i] = a[i];
+a[i] = t;
+}
+}
