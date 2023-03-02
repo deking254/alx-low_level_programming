d8508b826670e6c7784c0183171336931fa28555
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-01 21:53:41 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,7 +15,7 @@ while (str[i1])
 {
 for (i2 = 0; i2 <= 7; i2++)
 {
-if (str[i1] == leet[i2] || str[i1] -32 == leet[i2])
+if (str[i1] == leet[i2] || str[i1] - 32 == leet[i2])
 str[i1] = i2 + '0';
 }
 i1++;
