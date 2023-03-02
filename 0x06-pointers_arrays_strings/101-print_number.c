00af4a3128513d1f5428553e0b85322f348a04d2
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-01 22:54:14 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,2 +1,19 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+ * print_number - check the code
+ * @n: integer.
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+unsigned int num =n;
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+if ((num / 10) > 0)
+print_number(num / 10);
+_putchar((num % 10) + '0');
+}
