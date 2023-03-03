7ccf53d2e93353b52994e7c9cb866ffb7a3c657d
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-02 23:25:07 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,1 +1,38 @@
-#include 'main.h'
+#include <stdio.h>
+/**
+ * print_buffer - check the code
+ * @byt: integer.
+ * @size: integer.
+ * Return: Always 0.
+ */
+void print_buffer(char *b, int size)
+{
+int byt, i;
+for (byt = 0; byt < size; byt += 10)
+{
+printf("%08x: ", byt);
+for (i = 0; i < 10; i++)
+{
+if ((i + byt) >= size)
+printf(" ");
+else
+printf("%02x", *(b + i +byt));
+if ((i % 2) != 0 && i != 0)
+printf(" ");
+}
+for ( i = 0; i < 10; i++)
+{
+if ((i + byt) >= size)
+break;
+else if ((*b + i + byt) >= 31 && *(b + i + byt) <= 126)
+printf("%c", *(b + i + byt));
+else
+printf(".");
+}
+if (byt >= size)
+continue;
+printf("\n");
+}
+if (size <= 0)
+printf("\n");
+}
