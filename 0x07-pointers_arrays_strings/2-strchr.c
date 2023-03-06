11c277dfa84c9cb4c57b0550ac07539eb3c58996
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-06 01:58:14 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,26 @@
-#include
+#include "main.h"
+
+/**
+ * _strchr - prints buffer in hexa
+ * @s: the address of memory to print
+ * @c: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *_strchr(char *s, char c)
+{
+int i = 0;
+int j;
+
+while (s[i] != '\0')
+{
+i++;
+}
+for (j = 0; j < i; j++)
+{
+if (s[j] == c)
+break;
+}
+return (s + j);
+}
