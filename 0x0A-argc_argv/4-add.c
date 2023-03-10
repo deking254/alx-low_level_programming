37f4ae0892593515f1ed32f08b54b752bff39e8f
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-10 08:16:34 -0800

    as

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,14 +8,16 @@
  */
 int main(int argc, char *argv[])
 {
-int sum, i;
+int sum = 0, i;
 
 if (argc > 1)
 {
 for (i = 1; i < argc; i++)
 {
 if (*argv[i] >= 48 && *argv[i] <= 57)
+{
 sum += atoi(argv[i]);
+}
 else
 {
 printf("%s\n", "Error");
