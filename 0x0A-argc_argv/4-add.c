4f9dccf8b471dbc279d7a807ab38a4b29e0e0a5e
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-11 06:04:19 -0800

    abc

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,21 +8,24 @@
  */
 int main(int argc, char *argv[])
 {
-int sum = 0, i;
+long sum = 0, i;
+char *endptr;
+long val;
 
 if (argc > 1)
 {
 for (i = 1; i < argc; i++)
 {
-if (atoi(argv[i]) != 0)
-sum += atoi(argv[i]);
+val = strtol(argv[i], &endptr, 10);
+if (*endptr == '\0')
+sum += val;
 else
 {
 printf("%s\n", "Error");
 return (1);
 }
 }
-printf("%d\n", sum);
+printf("%ld\n", sum);
 }
 else
 printf("%d\n", 0);
