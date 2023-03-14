12279424caccdf55370754fb88ff64fc8bf5c35c
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-14 08:18:03 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,10 +11,13 @@ char *_strdup(char *str)
 {
 char *arr_ptr;
 int i = 0, j;
-
+if (str == NULL)
+return (NULL);
 while (str[i] != '\0')
 i++;
 arr_ptr = malloc(i + 1);
+if (arr_ptr == NULL)
+return (NULL);
 for (j = 0; str[j] != '\0'; j++)
 {
 arr_ptr[j] = str[j];
