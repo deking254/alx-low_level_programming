962f6fd9bfc4b60ed7793ce70b9b974bd70927ca
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-17 05:45:51 -0700

    files

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,8 +12,8 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 char *a;
+unsigned int i;
 
-a = ptr;
 if (new_size > old_size)
 {
 a = (char *)malloc(new_size);
@@ -28,14 +28,27 @@ if (new_size == old_size)
 {
 return (a);
 }
-if (a == NULL)
+if (ptr == NULL)
 {
 a = malloc(new_size);
-a[0] = 0;
+if (a == NULL)
+return (NULL);
+else
+return (a);
 }
-if (a != NULL && new_size == 0)
+if (new_size == 0)
 {
+free(ptr);
 return (NULL);
 }
+a = malloc(new_size);
+if (a == NULL)
+return (NULL);
+
+for (i = 0; i < old_size && i < new_size; i++)
+{
+a[i] = ((char *)ptr)[i];
+}
+free(ptr);
 return (a);
 }
