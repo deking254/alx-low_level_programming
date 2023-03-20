a8473dde27d003608526cdd31b42d1d265835244
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-20 12:10:55 -0700

    files

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,15 +10,15 @@ void print_dog(struct dog *d)
 if (d != NULL)
 {
 if (d->name != NULL)
-printf("%s\n", d->name);
+printf("Name: %s\n", d->name);
 else
 printf("Name: (nil)");
 if ((d->age) >= 0)
-printf("%f\n", d->age);
+printf("Age: %f\n", d->age);
 else
 printf("Age: (nil)");
 if (d->owner != NULL)
-printf("%s\n", d->owner);
+printf("Owner: %s\n", d->owner);
 else
 printf("Owner: (nil)");
 }
