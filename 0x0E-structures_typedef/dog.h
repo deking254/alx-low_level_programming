5f415f0037d125a44a62cfa2c55bd14cc5b897cb
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-20 05:49:02 -0700

    files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,19 @@
+#ifndef dog
+#define dog
+#define name
+#define age
+#define owner
+/**
+ * struct dog - Short description
+ * @name: tyr
+ * @age: fej
+ * @owner: ryey
+ * Description: Longer description
+ */
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+#endif
