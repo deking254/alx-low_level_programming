ec85d70b79b1665f92202421190f7bac2f58cdc8
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-20 09:40:37 -0700

    files

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,1 +1,25 @@
-d
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - check the code
+ * @d: integer operand.
+ * Return: Always int.
+ */
+void print_dog(struct dog *d)
+{
+if (d != NULL)
+{
+if (d->name != NULL)
+printf("%s\n", d->name);
+else
+printf("Name: (nil)");
+if (d->age != 0)
+printf("%.1f\n", d->age);
+else
+printf("Age: (nil)");
+if (d->owner != NULL)
+printf("%s\n", d->owner);
+else
+printf("Owner: (nil)");
+}
+}
