a3c9e1a49277c51fac9966a845f35eb6d31e0106
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-20 07:11:45 -0700

    files

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,1 +1,17 @@
-d
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - check the code
+ * @d: integer operand.
+ * @name: integer operand.
+ * @age: integer operand.
+ * @owner: integer operand.
+ * Return: Always int.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,1 +1,1 @@
-d
+void init_dog(struct dog *d, char *name, float age, char *owner);
