47f4da8808b8ce19dac35856f155e963c1dc65a2
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-21 07:52:49 -0700

    files

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,12 @@
-d
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - check the code
+ * @d: integer operand.
+ * Return: Always int.
+ */
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+free(d);
+}
