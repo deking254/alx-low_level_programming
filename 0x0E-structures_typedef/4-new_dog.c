5fc0a5fb8669bdb3f3a17a4e442b208b6fd19a51
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-21 09:23:24 -0700

    files

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,34 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
+ * _strlen - check the codeerand.
+ * @src: integer operand.
+ * Return: Always int.
+ */
+int _strlen(char *src)
+{
+int len = 0;
+while (src[len] != '\0')
+{
+len++;
+}
+return (len++);
+}
+/**
+ * _strcpy - check the code
+ * @dest: integer operand.
+ * @src: integer operand.
+ * Return: Always int.
+ */
+char *_strcpy(char *dest, const char *src)
+{
+int i;
+for (i = 0; src[i] != '\0'; i++)
+dest[i] = src[i];
+dest[i] = '\0';
+return (dest);
+}
+/**
  * new_dog - check the code
  * @name: integer operand.
  * @age: integer operand.
@@ -12,13 +40,14 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *p;
+int len = 0, leng = 0;
 char *namecopy;
 char *ownercopy;
 if (name != NULL)
 {
-namecopy = malloc(30);
+namecopy = malloc(_strlen(name));
 if (namecopy != NULL)
-strcpy(namecopy, name);
+_strcpy(namecopy, name);
 }
 else
 {
@@ -26,14 +55,14 @@ return (NULL);
 }
 if (owner != NULL)
 {
-ownercopy = malloc(sizeof(char) * strlen(owner));
+ownercopy = malloc(sizeof(char) * (_strlen(owner)));
 if (ownercopy != NULL)
-strcpy(ownercopy, owner);
+_strcpy(ownercopy, owner);
 }
 else
 return (NULL);
-p = malloc(sizeof(char) * strlen(name) + sizeof(age) +
-sizeof(char) * strlen(owner));
+p = malloc(sizeof(char) * (len++) + sizeof(age) +
+sizeof(char) * (leng++));
 if (p != NULL)
 {
 p->name = name;
@@ -41,7 +70,6 @@ p->age = age;
 p->owner = owner;
 }
 else
-free(p);
 return (NULL);
 return (p);
 }
