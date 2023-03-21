46112ec4e3a32815dc66e92bd67f414e3842a9a8
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-21 00:53:20 -0700

    files

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,1 +1,31 @@
-d
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - check the code
+ * @d: integer operand. 
+ * @name: integer operand.
+ * @age: integer operand.
+ * @owner: integer operand.
+ * Return: Always int.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *p;
+p =malloc(sizeof(char) * strlen(name)+ sizeof(age)+ sizeof(char) * strlen(owner));
+if (p != NULL)
+{
+p->name = name;
+p->age = age;
+p->owner = owner;
+}
+else
+return (NULL);
+/*
+printf("%s\n", p->name);
+printf("%f\n", p->age);
+printf("%s\n", p->owner);
+*/
+return (p);
+}
