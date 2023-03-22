5abe3c7ba4214424013270838f4f7fa6fb448059
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-22 03:37:51 -0700

    files

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,7 +11,8 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-
+if (array != NULL && cmp != NULL)
+{
 if (size <= 0)
 return (-1);
 for (i = 0; i < size; i++)
@@ -20,6 +21,7 @@ if (cmp(array[i]) != 0)
 {
 return (i);
 break;
+}
 }
 }
 return (-1);
