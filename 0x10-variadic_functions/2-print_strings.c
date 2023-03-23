60c0be77ef5c04ffad913ff33e769a7942f493ef
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-23 07:10:51 -0700

    files

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,10 +14,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 va_list po;
 unsigned int i;
 va_start(po, n);
+
 for (i = 0; i < n; i++)
 {
 printf("%s", va_arg(po, char *));
-if (i != (n - 1))
+if (i != (n - 1) && separator != NULL)
 printf("%s", separator);
 }
 printf("\n");
