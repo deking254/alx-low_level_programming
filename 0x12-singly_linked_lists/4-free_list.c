84e612a6603a8f90830a9d07ef41f11c25ad1456
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-30 13:48:41 -0700

    files

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,1 +1,19 @@
-d
+#include "lists.h"
+
+/**
+ * free_list - check the code
+ * @head: head.
+ * Return: Always 0.
+ */
+void free_list(list_t *head)
+{
+list_t *ar;
+list_t *save;
+
+for (ar = head; ar != NULL; )
+{
+save = ar->next;
+free(ar);
+ar = save;
+}
+}
