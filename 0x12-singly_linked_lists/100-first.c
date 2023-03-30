0606b23fb06a3d2aa1ebd45868cacb41c79c992e
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-03-30 14:22:13 -0700

    files

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,14 @@
-d
+#include <stdio.h>
+
+void first_printer(void) __attribute__ ((constructor));
+
+/**
+ * first_printer - prints a sentence before the main
+ * function is executed
+ */
+void first_printer(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
+
