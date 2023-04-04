7c6a7122a51ba0caaae8cfac04bbce3bc0d6ce6f
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-04 04:32:54 -0700

    files

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,1 +1,18 @@
-d
+#include "lists.h"
+
+/**
+ * sum_listint - check the code
+ * @head: hed.
+ * Return: Always 0.
+ */
+int sum_listint(listint_t *head)
+{
+int total = 0;
+
+while (head != NULL)
+{
+total += head->n;
+head = head->next;
+}
+return (total);
+}
