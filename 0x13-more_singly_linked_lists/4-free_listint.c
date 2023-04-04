1d62359d86f360d59f89838af0f7d9087d49f63e
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-03 23:59:45 -0700

    files

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,21 @@
-d
+#include "lists.h"
+
+/**
+ * free_listint - check the code
+ * @head: braiin.
+ * Return: Always 0.
+ */
+void free_listint(listint_t *head)
+{
+listint_t *save;
+save = head;
+if (head != NULL)
+{
+while (head->next != NULL)
+{
+head = head->next;
+free(save);
+save = head;
+}
+}
+}
