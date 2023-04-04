aa958c2c5914a026d87c6a6cb52a7a48d9cb5f1a
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-04 04:21:10 -0700

    files

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,1 +1,25 @@
-d
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - check the code
+ * @head: h
+ * @index: i
+ * Return: Always 0.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int i;
+listint_t *tmp;
+tmp = head;
+for (i = 0; i < index; i++)
+{
+if (tmp)
+tmp = tmp->next;
+else
+break;
+}
+if (tmp)
+return (tmp);
+else
+return (NULL);
+}
