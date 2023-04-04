d83e37f248137aca61737be008973cc9e1691bc3
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-04 03:45:01 -0700

    files

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,1 +1,21 @@
-d
+#include "lists.h"
+
+/**
+ * pop_listint - check the code
+ * @head: hed.
+ * Return: Always 0.
+ */
+int pop_listint(listint_t **head)
+{
+listint_t *sav;
+int i;
+
+if (*head)
+{
+i = (*head)->n;
+sav = (*head)->next;
+free(*head);
+*head = sav;
+}
+return (i);
+}
