7b7436e76845e81e047faf5ed003960761e691b1
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-04 00:39:08 -0700

    files

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,21 @@
-d
+#include "lists.h"
+
+/**
+ * free_listint2 - check the code
+ * @head: h
+ * Return: Always 0.
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *sav;
+listint_t *save;
+sav = *head;
+save = *head;
+while (sav != NULL)
+{
+sav = sav->next;
+free(save);
+save = sav;
+}
+*head = NULL;
+}
