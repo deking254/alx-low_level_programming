56b4bde01a01a51383fac9bdebef30cc1b604648
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-04 09:07:30 -0700

    files

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,1 +1,37 @@
-d
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - check the code
+ * @head: g
+ * @idx: h
+ * @n: n
+ * Return: Always 0.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+listint_t *ins;
+listint_t *tmp;
+unsigned int i;
+
+tmp = *head;
+ins = malloc(sizeof(listint_t));
+for (i = 0; i < idx; i++)
+{
+if (tmp != NULL)
+{
+if (i == (idx - 1))
+{
+ins->n = n;
+ins->next = tmp->next;
+tmp->next = ins;
+tmp->next = ins;
+}
+tmp = tmp->next;
+}
+else
+{
+break;
+}
+}
+return (tmp);
+}
