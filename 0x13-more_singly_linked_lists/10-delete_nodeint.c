527ad2f232a7e79930d2c5f5e1ea1ddd27a36fb5
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-05 01:09:19 -0700

    files

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,1 +1,37 @@
-d
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+unsigned int i;
+listint_t *tmp;
+listint_t *nextsave;
+tmp = *head;
+if (index == 0)
+{
+nextsave = tmp->next;
+free(tmp);
+*head = nextsave;
+}
+else
+{
+for (i = 0; i < index; i++)
+{
+if (i == (index - 1))
+{
+nextsave = tmp->next->next;
+free(tmp->next);
+tmp->next = nextsave;
+}
+tmp = tmp->next;
+}
+}
+if (tmp)
+return (1);
+else
+return (-1);
+}
--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,1 +1,11 @@
-d
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+
+}
