e2cd474e039d6d0733778e262dca203781fa73d5
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-06 05:48:59 -0700

    files

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,27 @@
-d
+#include "main.h"
+
+/**
+ * print_binary - check the code
+ * @n: n
+ * Return: Always 0.
+ */
+void print_binary(unsigned long int n)
+{
+int i, status = 0;
+for (i = 63; i >= 0; i--)
+{
+unsigned long m = 1UL << i;
+if ((n & m) == m)
+{
+printf("1");
+status = 1;
+}
+else
+{
+if (status == 1)
+printf("0");
+}
+}
+if (status == 0)
+printf("0");
+}
