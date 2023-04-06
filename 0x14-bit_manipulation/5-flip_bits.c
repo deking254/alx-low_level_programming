d0489c9ded49afb5a344027b32fcf26ca09893de
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-06 09:10:24 -0700

    files

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,1 +1,54 @@
-d
+#include "main.h"
+char *prnt_binary(unsigned long int n);
+/**
+ * flip_bits - check the code
+ * @n: n
+ * @m: m
+ * Return: Always 0.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+char *nptr;
+char *mptr;
+int i;
+unsigned int sum = 0;
+
+nptr = prnt_binary(n);
+mptr = prnt_binary(m);
+if (nptr != NULL && mptr != NULL)
+{
+for (i = 0; i <= 63; i++)
+{
+if (nptr[i] != mptr[i])
+sum += 1;
+}
+}
+return (sum);
+}
+/**
+ * prnt_binary - check the code
+ * @n: n
+ * Return: Always 0.
+ */
+char *prnt_binary(unsigned long int n)
+{
+int i;
+char *bitsptr;
+int ind = 0;
+bitsptr = malloc(64);
+for (i = 63; i >= 0; i--)
+{
+unsigned long m = 1UL << i;
+if ((n & m) == m)
+{
+bitsptr[ind] = '1';
+ind++;
+}
+else
+{
+bitsptr[ind] = '0';
+ind++;
+}
+}
+return (bitsptr);
+}
