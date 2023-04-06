1be5747c5e760fd44aec6a20deb097516fa6195a
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-06 06:21:40 -0700

    files

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,1 +1,36 @@
-d
+#include "main.h"
+
+/**
+ * get_bit - check the code
+ * @n: n
+ * @index: index
+ * Return: Always 0.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+int i;
+char *bits;
+unsigned long int m;
+int ind = 0;
+bits = malloc(64);
+for (i = 63; i >= 0; i--)
+{
+m = 1Ul << i;
+if (bits != NULL)
+{
+if (n & m)
+{
+bits[ind] = '1';
+ind++;
+}
+else
+{
+bits[ind] = '0';
+ind++;
+}
+}
+}
+if (bits[63 - index] == 48)
+return (0);
+return (1);
+}
