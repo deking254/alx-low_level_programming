6f944f0c9bde4953df7c21acfb74d56567e446b8
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-06 09:44:58 -0700

    files

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,20 @@
-d
+#include "main.h"
+
+/**
+ * get_endianness - check the code
+ *
+ * Return: Always 0.
+ */
+int get_endianness(void)
+{
+unsigned int number = 0x12345678;
+char *nptr = (char *)&number;
+if (*nptr == 0x78)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
