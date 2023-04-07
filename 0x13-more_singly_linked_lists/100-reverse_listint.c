29a94171c1e3620499e6e7fba35013867dec707d
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-07 09:27:43 -0700

    files

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,11 +1,24 @@
 #include "lists.h"
 
 /**
- * main - check the code
- *
+ * reverse_listint - check the code
+ * @head: h
  * Return: Always 0.
  */
 listint_t *reverse_listint(listint_t **head)
 {
-
+listint_t *nextsave = NULL;
+listint_t *previous = NULL;
+listint_t *current = *head;
+while (current != NULL)
+{
+nextsave = current->next;
+current->next = previous;
+previous = current;
+current = nextsave;
+}
+*head = previous;
+if (*head != NULL)
+printf("pree %d\n", (*head)->n);
+return (*head);
 }
