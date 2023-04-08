b313b45462fce01abe5fa04061177fb3dc1ffd07
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-08 16:24:40 -0700

    files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 ssize_t n, k;
 int fd;
-char *buffer;
+void *buffer;
 ssize_t i = 0;
 buffer = malloc(1024);
 if (buffer != NULL && filename != NULL)
