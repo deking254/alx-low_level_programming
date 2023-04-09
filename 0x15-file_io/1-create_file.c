800cebba0f3369ccffeeb2691f8ba0ce79b5168b
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-09 04:55:29 -0700

    files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,16 +10,15 @@ int create_file(const char *filename, char *text_content)
 {
 int fd;
 size_t length = 0;
-ssize_t l;
 mode_t perm = 0600;
+ssize_t l;
 if (filename != NULL)
 {
+fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, perm);
 if (text_content != NULL)
 {
 while (text_content[length] != '\0')
 length++;
-}
-fd = open(filename, O_CREAT | O_TRUNC, perm);
 if (fd != -1)
 {
 l = write(fd, text_content, length);
@@ -33,6 +32,12 @@ return (-1);
 }
 else
 return (-1);
+}
+else
+{
+close(fd);
+return (-1);
+}
 }
 return (-1);
 }
