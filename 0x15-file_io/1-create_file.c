f5251ec32822bfffae83c3aac77a602db1c58c6d
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-09 03:34:19 -0700

    files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,38 @@
-d
+#include "main.h"
+
+/**
+ * create_file - check the code
+ * @filename: file
+ * @text_content: text.
+ * Return: Always 0.
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+size_t length = 0;
+ssize_t l;
+mode_t perm = 0600;
+if (filename != NULL)
+{
+if (text_content != NULL)
+{
+while (text_content[length] != '\0')
+length++;
+}
+fd = open(filename, O_CREAT | O_TRUNC, perm);
+if (fd != -1)
+{
+l = write(fd, text_content, length);
+if (l != -1)
+{
+close(fd);
+return (1);
+}
+else
+return (-1);
+}
+else
+return (-1);
+}
+return (-1);
+}
