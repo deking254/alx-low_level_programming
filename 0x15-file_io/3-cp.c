d464d523949d41eef3160d633fbbf3c29fb2dd21
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-09 12:29:51 -0700

    files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,7 +14,7 @@ void *buffer[1024];
 mode_t perm = 0664;
 if (ac != 3)
 {
-dprintf(2, "Usage: cp file_from file_to\n");
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97); }
 fdfrom = open(av[1], O_RDWR, perm);
 fdto = open(av[2], O_RDWR | O_CREAT | O_TRUNC, perm);
@@ -28,20 +28,20 @@ c = close(fdto);
 cc = close(fdfrom);
 if (c == -1)
 {
-dprintf(2, "Error: Can't close fd  %d\n", fdto);
+dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", fdto);
 exit(100); }
 if (cc == -1)
 {
-dprintf(2, "Error: Can't close fd  %d\n", fdfrom);
+dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", fdfrom);
 exit(100); }
 }
 else
 {
-dprintf(2, "Error: Can't write to  %s\n", av[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", av[2]);
 exit(99); }
 }
 else
 {
-dprintf(2, "Error: Can't read from file %s\n", av[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 exit(98); }
 return (0); }
