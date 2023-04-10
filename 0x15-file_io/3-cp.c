403433f0a5edb51ea472e1dd55b7a5e11d6dfae9
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-10 01:05:05 -0700

    files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,34 +14,63 @@ char buffer[1024];
 mode_t perm = 0664;
 if (ac != 3)
 {
-exit(97);
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");}
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97); }
+if (av[1] != NULL)
+{
 fdfrom = open(av[1], O_RDWR);
-fdto = open(av[2], O_RDWR |O_CREAT | O_TRUNC, perm);
+if (fdfrom != -1)
+{
 n = read(fdfrom, buffer, 1024);
-if (av[1] != NULL && n != -1 && fdfrom != -1)
+if (n != -1)
+{
+fdto = open(av[2], O_RDWR | O_CREAT | O_TRUNC, perm);
+if (fdto != -1)
 {
 l = write(fdto, buffer, n);
-if (l != -1 && fdto != -1)
+if (l != -1)
 {
-c = close(fdto);
-cc = close(fdfrom);
+c = close(fdfrom);
+cc = close(fdto);
 if (c == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", fdto);
-exit(100); }
+dprintf(STDERR_FILENO,"Error: Can't close fd %u\n", fdfrom);
+exit(100);
+}
 if (cc == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", fdfrom);
-exit(100); }
+dprintf(STDERR_FILENO,"Error: Can't close fd %u\n", fdto);
+exit(100);
+}
 }
 else
 {
-dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", av[2]);
-exit(99); }
+dprintf(STDERR_FILENO,"Error: Can't Can't write to %s\n", av[2]);
+exit(98);
+}
 }
 else
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-exit(98); }
-return (0); }
+dprintf(STDERR_FILENO,"Error: Can't Can't write to %s\n", av[2]);
+exit(98);
+}
+}
+else
+{
+dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", av[1]);
+exit(98);
+}
+}
+else
+{
+dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", av[1]);
+exit(98);
+}
+}
+else
+{
+dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", av[1]);
+exit(98);
+}
+return (0);
+}
