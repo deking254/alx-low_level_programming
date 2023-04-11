440f19cfef2594ec885ef6bc5f9af808041da2f8
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-04-11 06:38:51 -0700

    files

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,1 +1,17 @@
-d
+#include <libelf.h>
+#include "main.h"
+
+int main (int argc, char *[]argv)
+{
+int fd;
+ELF *e;
+fd = open(argv[1], O_RDONLY);
+e = elf_begin(fd, ELF_C_READ, NULL);
+Elf64_Ehdr *elfhdr64 = elf64_getehdr(e);
+if (elfhdr64 != NULL)
+{
+printf("Entry point: 0x%x\n", elfhdr32->e_entry);
+}
+return (0);
+}
+
