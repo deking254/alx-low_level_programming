0be587adb48f890f59af91e831cd908660a78c46
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-18 12:48:57 -0700

    5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,1 +1,27 @@
-d
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - check the code
+ * @head: hed
+ * @index: i
+ * Return: Always EXIT_SUCCESS.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+unsigned int i = 0;
+while (head)
+{
+if (i != index)
+{
+if (head != NULL)
+head = head->next;
+else
+break;
+}
+else
+return (head);
+i++;
+}
+return (NULL);
+}
