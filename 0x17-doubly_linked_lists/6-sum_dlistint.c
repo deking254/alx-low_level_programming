a55b37990a8d32798accc69cbf4c919dc82efeb0
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-18 12:58:28 -0700

    6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,18 @@
-d
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - check the code
+ * @head: h
+ * Return: Always EXIT_SUCCESS.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+int sum = 0;
+while (head)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
+}
