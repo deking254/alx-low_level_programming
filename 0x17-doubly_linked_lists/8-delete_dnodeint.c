7dc9faa49a3e6407071251d2022df400419113da
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-19 05:55:55 -0700

    8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,1 +1,52 @@
-d
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - check the code
+ * @head: hed
+ * @index: index
+ * Return: Always EXIT_SUCCESS.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+unsigned int i = 0;
+dlistint_t *ne = *head;
+if (*head != NULL)
+while (ne)
+{
+if (i == index)
+{
+if (index == 0)
+{
+if (ne->next)
+{
+ne = ne->next;
+free(ne->prev);
+*head = ne;
+}
+else
+{
+free(ne);
+*head = NULL;
+}
+return (1);
+}
+if (ne->next)
+{
+ne->next->prev = ne->prev;
+ne->prev->next = ne->next;
+free(ne);
+return (1);
+}
+else
+{
+free(ne);
+return (1);
+}
+}
+ne = ne->next;
+i++;
+}
+return (-1);
+}
