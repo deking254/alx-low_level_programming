fceec6fee0b8ac2ebbb7841461a101088735608e
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-06-08 09:05:39 -0700

    all

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -10,9 +10,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+char *key;
+char *value;
+struct hash_node_s *next;
 } hash_node_t;
 /**
  * struct hash_table_s - Hash table data structure
@@ -24,8 +24,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+unsigned long int size;
+hash_node_t **array;
 } hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
@@ -34,10 +34,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-/*shash_table_t *shash_table_create(unsigned long int size);
-int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shash_table_t *ht, const char *key);
-void shash_table_print(const shash_table_t *ht);
-void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);*/
+/*
+* shash_table_t *shash_table_create(unsigned long int size);
+* int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+* char *shash_table_get(const shash_table_t *ht, const char *key);
+* void shash_table_print(const shash_table_t *ht);
+* void shash_table_print_rev(const shash_table_t *ht);
+* void shash_table_delete(shash_table_t *ht);
+*/
 #endif
