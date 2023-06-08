53ec8cc0164508abc8084bc214cd602f4da68bc1
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-06-08 09:28:46 -0700

    1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,16 @@
+/**
+ * hash_djb2 - check the code
+ * @str: string
+ * Return: Always EXIT_SUCCESS.
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
-
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+unsigned long int hash;
+int c;
+hash = 5381;
+while ((c = *str++))
+{
+hash = ((hash << 5) + hash) +c; /* hash * 33 + c */
+}
+return (hash);
 }
