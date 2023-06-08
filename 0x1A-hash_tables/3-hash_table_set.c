757c276882a4c78088b395ae4e987b10ea8cb59c
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-06-08 09:36:58 -0700

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,8 +9,9 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+unsigned long int index = key_index((const unsigned char *)key, 1024);
 hash_node_t *hd;
-hd = ht->array[0];
+hd = ht->array[index];
 hd->key = (char *)key;
 hd->value = (char *)value;
 return (1);
