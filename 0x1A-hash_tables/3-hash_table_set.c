170a1deefbf9503ae425ea519811ebe0347fd823
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-06-09 00:18:20 -0700

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * hash_table_set - check the code
  * @key: key
@@ -10,9 +11,17 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int index = key_index((const unsigned char *)key, 1024);
-hash_node_t *hd;
-hd = ht->array[index];
-hd->key = (char *)key;
-hd->value = (char *)value;
+hash_node_t **hd;
+hash_node_t *new;
+new = malloc(sizeof(hash_node_t));
+if (new)
+{
+new->key = (char *)key;
+new->value = (char *)value;
+new->next = NULL;
+}
+hd = ht->array;
+hd[index] = new;
+printf("%s\n", hd[index]->key);
 return (1);
 }
