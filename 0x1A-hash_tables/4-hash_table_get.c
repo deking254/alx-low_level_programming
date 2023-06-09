9107f07053eaeb606f4a7ddbad3459ce95e8df25
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-06-09 09:27:54 -0700

    3

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,1 +1,35 @@
-d
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - check the code
+ * @ht: table
+ * @key: key
+ * Return: Always EXIT_SUCCESS.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+unsigned long int i = 0;
+if (ht)
+{
+while (i < ht->size)
+{
+if (ht->array != NULL)
+{
+if (ht->array[i] != NULL)
+{
+if (strcmp(ht->array[i]->key, key) == 0)
+return (ht->array[i]->value);
+}
+}
+else
+return (NULL);
+i++;
+}
+}
+else
+return (NULL);
+return (NULL);
+}
