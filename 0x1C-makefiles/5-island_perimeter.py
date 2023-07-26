a7d76ed715582e0e30140fa5316b163f421b3f19
deking254 <dekingsky@gmail.com>
2023-07-26 16:22:59 +0000

    5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,8 @@
-d
+#!/usr/bin/python3
+def island_perimeter(grid):
+    perimeter = 0
+    for j in grid:
+        for i in j:
+            if i == 1:
+                perimeter += 2
+    return (perimeter + 2)
