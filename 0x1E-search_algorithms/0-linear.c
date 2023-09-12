bb62a060a256bf35f6c8ceead4096c0e26949d86
deking254 <dekingsky@gmail.com>
2023-09-12 12:37:27 +0000

    0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,6 +1,6 @@
 #include "search_algos.h"
 #include <stdlib.h>
-#include <stdio.h>
+void print_linear(int value);
 /**
  * linear_search  - Entry point
  * @array: arr
@@ -15,16 +15,47 @@ size_t i;
 int status;
 for (i = 0; i < size; i++)
 {
+print_linear(array[i]);
 if (array[i] == value)
 {
 status = 1;
 break;
 }
 if (i == size - 1)
+{
 status = 0;
 }
+}
 if (status == 1)
+{
 return (i);
+}
 else
 return (-1);
+}
+
+/**
+ * print_linear  - Entry point
+ * @value: value searched
+ * Return: Always EXIT_SUCCESS
+ */
+void print_linear(int value)
+{
+char str[20];
+fputs("Value checked array = [", stdout);
+if (value < 0)
+{
+putchar('-');
+snprintf(str, 20, "%d", value * -1);
+fputs(str, stdout);
+}
+if (value > 0)
+{
+snprintf(str, 20, "%d", value);
+fputs(str, stdout);
+}
+if (value == 0)
+putchar('0');
+putchar(']');
+putchar('\n');
 }
--- 0x1E-search_algorithms/print_array.c
+++ 0x1E-search_algorithms/print_array.c
@@ -1,39 +1,11 @@
 #include "search_algos.h"
 #include <stdlib.h>
-#include <stdio.h>
-
-void print_array(int *array, size_t end, size_t current_index);
 /**
- * binary_search - Entry point
- * @array: arr
- * @size:size of arr
- * @value: search
+ * print_array - Entry point
+ * @end: arr
+ * @current_index:size of arr
  * Return: Always EXIT_SUCCESS
  */
-
-int binary_search(int *array, size_t size, int value)
-{
-size_t index;
-size_t start_index = 0;
-size_t end_index = size - 1;
-print_array(array, end_index, start_index);
-while (start_index <= end_index)
-{
-index = start_index + (end_index - start_index) / 2;
-if (array[index] == value)
-return (index);
-if (array[index] > value)
-{
-end_index = index - 1;
-}
-else
-start_index = index + 1;
-if (start_index <= end_index)
-print_array(array, end_index, start_index);
-}
-return (-1);
-}
-
 void print_array(int *array, size_t end, size_t current_index)
 {
 size_t i = 0;
