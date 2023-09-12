808e5f1bcb641b1dd58f92e8452b08fd97bb3a30
deking254 <dekingsky@gmail.com>
2023-09-12 13:56:14 +0000

    0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,6 @@
 #include "search_algos.h"
 #include <stdlib.h>
+void print_linear(int value, int index);
 /**
  * linear_search  - Entry point
  * @array: arr
@@ -33,4 +34,38 @@ return (i);
 }
 else
 return (-1);
+}
+return (-1);
+}
+
+/**
+ * print_linear  - Entry point
+ * @value: value searched
+ * @index: i
+ * Return: Always EXIT_SUCCESS
+ */
+void print_linear(int value, int index)
+{
+char str[20];
+char ind[20];
+fputs("Value checked array[", stdout);
+snprintf(ind, 20, "%d", index);
+fputs(ind, stdout);
+putchar(']');
+fputs(" = [", stdout);
+if (value < 0)
+{
+putchar('-');
+snprintf(str, 20, "%d", value * -1);
+fputs(str, stdout);
+}
+if (value > 0)
+{
+snprintf(str, 20, "%d", value);
+fputs(str, stdout);
+}
+if (value == 0)
+putchar('0');
+putchar(']');
+putchar('\n');
 }
