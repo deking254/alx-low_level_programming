a00a34290c38b644b140124bce9c6532749c2171
deking254 <dekingsky@gmail.com>
2023-09-14 13:20:24 +0000

    100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,6 +1,7 @@
 #include <math.h>
 #include "search_algos.h"
 void print_value(size_t prev, size_t step);
+void print_linear(int value, int index);
 /**
  * jump_search - Entry point
  * @array: arr
@@ -14,8 +15,6 @@ int jump_search(int *array, size_t size, int value)
 size_t step = sqrt(size);
 size_t prev = 0;
 size_t i;
-char ind[20];
-char val[20];
 while (array[prev] < value)
 {
 print_linear(array[prev], prev);
@@ -51,12 +50,46 @@ return (-1);
  */
 void print_value(size_t prev, size_t step)
 {
+char ind[20];
+char val[20];
 fputs("Value found between indexes [", stdout);
 snprintf(ind, 20, "%ld", prev - step);
 snprintf(val, 20, "%ld", prev);
 fputs(ind, stdout);
 fputs("] and [", stdout);
 fputs(val, stdout);
+putchar(']');
+putchar('\n');
+}
+
+/**
+ * print_linear  - Entry point
+ * @value: value searched
+ * @index: i
+ * Return: Always EXIT_SUCCESS
+ */
+void print_linear(int value, int index)
+{
+char str[20];
+char ind[20];
+fputs("Value checked array[", stdout);
+snprintf(ind, 20, "%d", index);
+fputs(ind, stdout);
+putchar(']');
+fputs(" = [", stdout);
+if (value < 0)
+{
+putchar('-');
+snprintf(str, 20, "%d", value * -1);
+fputs(str, stdout);
+}
+if (value > 0)
+{
+snprintf(str, 20, "%d", value);
+fputs(str, stdout);
+}
+if (value == 0)
+putchar('0');
 putchar(']');
 putchar('\n');
 }
